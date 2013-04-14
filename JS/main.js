438e3ea59a19fcf6a869b489fb2720a9ece77d1e
Maartenf <maartenforrier@hotmail.com>
2013-04-14 20:14:39 +0200

    Add initializeTiles function.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-	// ...
+	var canvasContext = $("canvas").getContext("2d");
+
+	var mapTiles;
+	
+	// Initialize and draw the tiles into the canvas.
+	function initializeTiles(mapTiles) {
+	
+		for (var i = 0; i < canvas.width / 20; i++) {
+		
+			for (var y = 0; y < canvas.height / 20; y++) {
+			
+				// ...
+			}
+		}
+	}
 });
