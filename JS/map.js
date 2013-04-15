a525d0996d2fb13d2f51bb10a657459b71d6307e
Maartenf <maartenforrier@hotmail.com>
2013-04-15 17:38:55 +0200

    Add new class map.

---
+++ JS/map.js
@@ -0,0 +1,14 @@
+/* Functions for initializing tiles
+   File: map.js */
+
+// Initialize and draw the tiles into the canvas.
+function map(mapTiles) {
+	
+		for (var i = 0; i < canvas.width / 20; i++) {
+		
+			for (var y = 0; y < canvas.height / 20; y++) {
+			
+				// ...
+			}
+		}
+	}
