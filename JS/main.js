f7c4712ded055ff5199ebe79b4e7f4d9ce6344d8
Maartenf <maartenforrier@hotmail.com>
2013-04-15 17:39:43 +0200

    Changed stuff to external files.

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,8 @@
+var canvasContext = $("canvas").getContext("2d");
+
+var mapTiles;
+
 $(document).ready(function() {
-
-	var canvasContext = $("canvas").getContext("2d");
-
-	var mapTiles;
 	
-	// Initialize and draw the tiles into the canvas.
-	function initializeTiles(mapTiles) {
-	
-		for (var i = 0; i < canvas.width / 20; i++) {
-		
-			for (var y = 0; y < canvas.height / 20; y++) {
-			
-				// ...
-			}
-		}
-	}
+	var map = new Map(mapTiles);
 });
